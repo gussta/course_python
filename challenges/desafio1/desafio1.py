73dce051707ae11583256fab974a8a9bfe28b31d
Gustavo Almeida <gussta.almeida@gmail.com>
2022-03-17 08:54:47 -0300

    Projeto: Descubra a Palavra e alguns exercicios do curso

---
+++ challenges/desafio1/desafio1.py
@@ -0,0 +1,14 @@
+"""
+***DESAFIO***
+"""
+
+nome = 'Gustavo Almeida Santos'
+idade = 23
+altura = 1.83
+peso = 90
+ano_atual = 2022
+imc = peso / (altura ** 2)
+
+print(f'{nome} nasceu no ano de {ano_atual-idade}, possui {idade} anos e tem {altura}m de altura. Devido ao seu peso ser {peso}kg, seu imc é de {imc:.2f}')
+
+
---
+++ challenges/desafio2/desafio2.py
@@ -0,0 +1,11 @@
+"""
+v1 = 0
+v2 = 10
+while v1 <=8:
+    print(v1, v2)
+    v1 += 1
+    v2 -= 1
+"""
+
+for r, v1 in enumerate(range(10, 1, -1)):
+    print(r, v1)
